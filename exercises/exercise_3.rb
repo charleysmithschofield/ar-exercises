eb4fb9aef1c5fae1a4bbef86810a06ffca2255ae
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-28 21:48:14 -0700

    exercise 3 complete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Load the third store
+@store3 = Store.find(3)
+
+# Use destroy method to delete the store from the database
+@store3.destroy
+
+# Verify the store has been deleted by outputting the count 
+puts "Number of stores: #{Store.count}"
