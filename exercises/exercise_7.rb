e943a60ee037249f378a1f09d0541f4a3a674a31
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-28 22:38:11 -0700

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,18 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Attempt to create a store with incomplete data
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+# Attempt to save the store and display errors if any
+if new_store.save
+  puts "Store '#{store_name}' successfully created!"
+else
+  puts "Failed to create store. Errors:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
